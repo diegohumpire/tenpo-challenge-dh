db20a98f0edd8c7e7ec4ac9d3e8db8a5e1cc54f0
Diego Humpire <humpire.diego@gmail.com>
2026-04-28 00:54:47 -0500

    Init

---
+++ src/main/java/com/tenpo/dh/challenge/dhapi/DhapiApplication.java
@@ -0,0 +1,13 @@
+package com.tenpo.dh.challenge.dhapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhapiApplication.class, args);
+	}
+
+}
